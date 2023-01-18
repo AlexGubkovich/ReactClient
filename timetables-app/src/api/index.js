4d41ee197aed607040246132da353bf64cc06e69
Alex Hubkovich <alex.gubkovich@gmail.com>
2023-01-18 18:43:41 +0300

    update api url

--- timetables-app/src/api/index.js
+++ timetables-app/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-export const BASE_URL = 'https://mytapp.azurewebsites.net';
+export const BASE_URL = 'https://mytapp.azurewebsites.net/';
 
 export const ENDPOINTS = {
     getGroups: "Groups",
